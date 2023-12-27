95f50fc03c03710759bb74364fff004e382f463e
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-12-26 22:34:49 -0300

    Subindo para o repositório remoto as criações e modificações

--- reservas/src/main/java/ifrn/pi/reservas/reservas/Repositories/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package ifrn.pi.reservas.reservas.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ifrn.pi.reservas.reservas.Models.Aluno;
-public interface AlunoRepository extends JpaRepository<Aluno, Long>  {
-    
-}
--- reservas/src/main/java/ifrn/pi/reservas/reservas/Repositories/MotoristaRepository.java
+++
@@ -1,9 +0,0 @@
-package ifrn.pi.reservas.reservas.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ifrn.pi.reservas.reservas.Models.Motorista;
-
-public interface MotoristaRepository extends JpaRepository<Motorista, Long>  {
-    
-}
